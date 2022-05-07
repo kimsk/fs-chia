ce9aef81faabb4873f41aa57ce76c5a48af637f8
Karlkim Suwanmongkol <contact@karlk.im>
2022-05-07 21:56:38 +0700

    try-chia-dotnet

---
+++ try-chia-dotnet/Program.fs
@@ -0,0 +1,10 @@
+﻿open chia.dotnet
+
+let endpoint = Config.Open("/home/karlkim/.chia/testnet/config/config.yaml").GetEndpoint("full_node")
+printfn "%s" endpoint.Uri.AbsoluteUri
+let rpcClient = new HttpRpcClient(endpoint)
+
+let fullNode = new FullNodeProxy(rpcClient, "unit_tests")
+let state = fullNode.GetBlockchainState() |> Async.AwaitTask |> Async.RunSynchronously
+let mempoolSize = state.MempoolSize
+printfn "%A" state 
